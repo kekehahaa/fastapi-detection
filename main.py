51f8ed3ff7dd544b6de67ce8e5d20a350810f889
Дмитрий  Муравский <kekehaha@Dmitrijs-MacBook-Air.local>
2024-10-27 17:56:15 +0300

    new

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import uvicorn
 
 from fastapi import FastAPI
-from app.api.endpoints.users import user_router
+from app.api.endpoints.files import file_router
 
 app = FastAPI()
 
-app.include_router(user_router)
+app.include_router(file_router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host='127.0.0.1', port=8000)
